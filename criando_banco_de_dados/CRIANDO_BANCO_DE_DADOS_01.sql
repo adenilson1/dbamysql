bb84d605c116aceb3d9dd6ccf125c75b7a99d8bf
adenilson1 <adenilson99@hotmail.com>
2025-02-18 11:06:25 -0300

    first commit

---
+++ criando_banco_de_dados/CRIANDO_BANCO_DE_DADOS_01.sql
@@ -0,0 +1,24 @@
+01-BANCO DE DADOS
+
+01.1-COMANDO DELETAR BANCO DE DADOS
+
+DROP DATABASE NOME_DO_BANCO
+
+01.2-COMANDO CRIANDO UM BANCO DE DADOS -> LIVRARIA
+
+CREATE DATABASE LIVRARIA;
+
+01.3-COMANDO MOSTRAR BANCOS DE DADOS
+
+SHOW DATABASES;
+
++--------------------+
+| Database           |
++--------------------+
+| LIVRARIA           |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+
